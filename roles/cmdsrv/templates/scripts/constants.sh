941660a09e1cbc099a08e3cb502aeff8eb712505
Tim Moody <tim@timmoody.com>
2017-12-18 08:55:34 -0500

    oer2go_install_move.sh and source of constants

---
+++ roles/cmdsrv/templates/scripts/constants.sh
@@ -0,0 +1,7 @@
+# Constants to source into admin console scripts
+
+WWWROOT={{ doc_root }}
+MODULES_DIR={{ iiab_modules_dir }}
+OER2GO_WORKING=/library/working/rachel/
+ZIM_DIR=/library/zims/
+ZIM_WORKING=/library/working/zims/
