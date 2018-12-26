2bbddacef42600692f416ee51f70f6b8431abe8d
George Hunt <georgejhunt@gmail.com>
2018-12-26 19:16:32 +0000

    change to accomodate new structure to zim_version_idx

--- roles/js-menu/files/menu-files/js/js-menu.js
+++ roles/js-menu/files/menu-files/js/js-menu.js
@@ -280,8 +280,9 @@ function procMenuItem(module) {
 function calcZimLink(module){
 	// if kiwix_url is defined use it otherwise use port
 	var href = '';
-	if(typeof zimVersions[module.zim_name] != 'undefined'){
-	  href =  zimVersions[module.zim_name] + '/';
+	if(typeof zimVersions[module.zim_name] != 'undefined' &&
+        typeof zimVersions[module.zim_name].zimFileName != 'undefined' ){
+	  href =  zimVersions[module.zim_name].zimFileName + '/';
   	if ( menuConfig.hasOwnProperty('kiwixUrl'))
       href = menuConfig.kiwixUrl + href;
     else
