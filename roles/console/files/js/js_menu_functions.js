625eda182647b773b636acd0bd9843be16832a7e
Tim Moody <tim@timmoody.com>
2018-12-31 16:44:48 -0500

    display available menu items

--- roles/console/files/js/js_menu_functions.js
+++ roles/console/files/js/js_menu_functions.js
@@ -2,12 +2,91 @@
 // copyright 2019 Tim Moody
 
 var jsMenuUrl = "/js-menu";
+var jsMenuImageUrl = "/js-menu/menu-files/images/";
+var jsMenuItemDefUrl = "/js-menu/menu-files/menu-defs/"
 var menuItemDefList = [];
+var menuItemDefs = {};
+menuItemDefs['call_count'] = 0;
 
 function getMenuItemDefList(){
 	return sendCmdSrvCmd("GET-MENU-ITEM-DEF-LIST", procMenuItemDefList);
 }
 
 function procMenuItemDefList (data){
+	var html = "";
 	menuItemDefList = data;
+	html = createMenuItemScaffold(menuItemDefList, "all-items");
+	$("#menusDefineMenuAllItemList").html(html);
+	for (var i = 0; i < menuItemDefList.length; i++) {
+		getMenuItemDef(menuItemDefList[i], "all-items")
+	}
+}
+
+function createMenuItemScaffold(list, prefix){
+  var html = "";
+  for (var i = 0; i < list.length; i++) {
+  	var menu_item_name = list[i];
+  	html += '<div id="' + prefix + '-' + menu_item_name + '" class="flex-row content-item" dir="auto">&emsp;Attempting to load ' + menu_item_name + ' </div>';
+  }
+  return html;
+}
+
+function getMenuItemDef(menuItem, prefix) {
+	var divId = prefix + '-' + menuItem;
+	menuItemDefs['call_count'] += 1;
+
+	var resp = $.ajax({
+		type: 'GET',
+		async: true,
+		url: jsMenuItemDefUrl + menuItem + '.json',
+		dataType: 'json'
+	})
+	.done(function( data ) {
+		menuItemDefs[menuItem] = data;
+		genMenuItem(divId, menuItemDefs[menuItem]);
+		checkMenuDone();
+	})
+	.fail(function (jqXHR, textStatus, errorThrown){
+		var menuHtml = '<div class="content-item" style="padding:10px; color: red; font-size: 1.5em">' + menuItem + ' - file not found or improperly formatted</div>';
+		$("#" + divId).html(menuHtml);
+		checkMenuDone();
+		jsonErrhandler (jqXHR, textStatus, errorThrown); // probably a json error
+	});
+	return resp;
+}
+
+function genMenuItem(divId, module) {
+  var menuHtml = "";
+	var langClass = "";
+	var menuItemDivId = "#" + divId;
+	var langLookup = langCodesXRef[module.lang]
+
+  if (selectedLangs.length > 0 && selectedLangs.indexOf(langLookup) == -1) { // not a selected language
+		$(menuItemDivId).hide();
+		return;
+	}
+
+	menuHtml+='<div class="content-icon">';
+	menuHtml+='<img src="' + jsMenuImageUrl + module.logo_url + '"></div>';
+
+	// item right side
+	menuHtml+='<div class="flex-col">';
+	menuHtml+='<div class="content-cell">';
+	// title
+	menuHtml+='<div class="content-item-title">';
+	menuHtml+=module.title;
+	menuHtml+='</div>'; // end content-item-title
+	menuHtml+='</div></div>';
+
+	$("#" + divId).html(menuHtml);
+}
+
+function checkMenuDone(){
+	menuItemDefs['call_count'] -= 1;
+	consoleLog (menuItemDefs['call_count']);
+	if (menuItemDefs['call_count'] == 0){
+		//genLangSelector();
+		//activateButtons();
+		//alert ("menu done");
+	}
 }
