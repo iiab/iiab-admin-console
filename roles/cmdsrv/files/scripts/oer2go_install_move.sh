86b915f18355f87b8ad78a5a1e613ae6c27f3b90
George Hunt <georgejhunt@gmail.com>
2019-01-13 19:15:54 +0000

    update the menu, look for the date clobber

--- roles/cmdsrv/files/scripts/oer2go_install_move.sh
+++ roles/cmdsrv/files/scripts/oer2go_install_move.sh
@@ -9,7 +9,7 @@ echo "Moving downloaded $1 to production"
 mv $OER2GO_WORKING$1 $MODULES_DIR
 
 # this is no longer needed due to get_oer2go_stat(cmd_info) and clobbers date
-#echo "Updating catalog"
-#iiab-get-oer2go-cat -v --no_download
+echo "Updating catalog"
+iiab-get-oer2go-cat -v --no_download --menu
 
 exit 0
