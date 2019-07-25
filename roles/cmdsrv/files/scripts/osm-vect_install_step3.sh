b9b66e79652403417aa4e64dc5011813cd594e1b
A Holt <holta@users.noreply.github.com>
2019-07-25 11:25:41 -0400

    Update osm-vect_install_step3.sh

--- roles/cmdsrv/files/scripts/osm-vect_install_step3.sh
+++ roles/cmdsrv/files/scripts/osm-vect_install_step3.sh
@@ -9,11 +9,11 @@
 
 # 2019-07-23: Temporary patch for IIAB 7.0 release, to reduce browser crashes
 # on low-memory client devices: https://github.com/iiab/iiab/issues/1728
-cd $1
+#cd $1
 #if [ $(wc -c < main.js) -eq 3300000 ]; then    # Check for current/problematic main.js which is exactly 3,300,000 bytes
 echo "Swapping 3MB $1/main.js for a newer one, to help with low-memory client devices"
-rm main.js
-wget https://raw.githubusercontent.com/iiab/maps/master/osm-source/regional-base/build/main.js    # This newer main.js is 3,300,039 bytes as of 2019-07-23
+cp -p $1/main.js $1/main.js.old
+wget https://raw.githubusercontent.com/iiab/maps/master/osm-source/regional-base/build/main.js -O $1/main.js    # This newer main.js is 3,300,039 bytes as of 2019-07-23
 #fi
 
 echo "Moving downloaded $1 to production"
