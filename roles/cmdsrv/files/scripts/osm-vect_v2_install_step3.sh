656c2956e7ddb8809349cd29cc183066e3ab8f47
Tim Moody <tim@timmoody.com>
2020-09-29 15:54:02 -0400

    conditionally remove preview files

--- roles/cmdsrv/files/scripts/osm-vect_v2_install_step3.sh
+++ roles/cmdsrv/files/scripts/osm-vect_v2_install_step3.sh
@@ -7,6 +7,9 @@
 
 # these should be soft coded
 
+PREVIEW_SAT=satellite_z0-z6_v3.mbtiles
+PREVIEW_OSM=planet_z0-z6_2019.mbtiles
+
 download_dir='/library/working/maps'
 tiles_dir='/library/www/osm-vector-maps/viewer/tiles'
 
@@ -15,6 +18,14 @@ mv $download_dir/$1 $tiles_dir
 if [ $? -ne 0 ]; then
 echo "Moving downloaded $1 to production FAILED"
 exit 1
+fi
+
+# if the PREVIEW tiles are in place, delete them
+if [ -e "/library/www/osm-vector-maps/viewer/tiles/$PREVIEW_OSM" ]; then
+   unlink "/library/www/osm-vector-maps/viewer/tiles/$PREVIEW_OSM"
+fi
+if [ -e "/library/www/osm-vector-maps/viewer/tiles/$PREVIEW_SAT" ]; then
+   rm -f "/library/www/osm-vector-maps/viewer/tiles/$PREVIEW_SAT"
 fi
 
 exit 0
