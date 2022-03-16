1877780e3018b90b597d0bffe393ae7172030d4c
tim-moody <tim@timmoody.com>
2022-03-16 17:06:25 -0400

    use perl crypt for password hashing

--- roles/cmdsrv/files/iiab-cmdsrv3.py
+++ roles/cmdsrv/files/iiab-cmdsrv3.py
@@ -2807,12 +2807,10 @@ def change_password(cmd_info):
         return resp
 
     # check old password - N.B. allows password guessing
-    readpasswd = spwddb[1]
-    pwparts = readpasswd.split('$')
-    salt = '$' + pwparts[1] + '$' +  pwparts[2] +'$'
-    calcpasswd = crypt.crypt(oldpasswd, salt)
+    read_pw_hash = spwddb[1]
+    calc_passwd_hash = get_password_hash(oldpasswd, read_pw_hash)
 
-    if calcpasswd != readpasswd:
+    if calc_passwd_hash != read_pw_hash:
         resp = cmd_error(cmd=cmd_info['cmd'], msg='Old Password Incorrect.')
         return resp
 
@@ -2829,7 +2827,11 @@ def change_password(cmd_info):
         return resp
 
     # create new password hash
-    newhash = crypt.crypt(newpasswd, salt)
+    newhash = get_password_hash(newpasswd, read_pw_hash)
+    if not newhash:
+        resp = cmd_error(cmd=cmd_info['cmd'], msg='Failed to hash New Password.')
+        return resp
+
     pwinput = user + ':' + newhash + '\n'
     pwinput = pwinput.encode() # convert to byte
 
@@ -2866,17 +2868,18 @@ def check_password_match(user, password):
 
     # check old password - N.B. allows password guessing
     passwd_hash = spwddb[1]
-    pwparts = passwd_hash.split('$')
-    if len (pwparts) > 1:
-        salt = '$' + pwparts[1] + '$' +  pwparts[2] +'$'
-    else:
-        return False # no salt so must not match
-
-    calc_passwd_hash = crypt.crypt(password, salt)
+    calc_passwd_hash = get_password_hash(password, passwd_hash)
     if calc_passwd_hash != passwd_hash:
         return False
     else:
         return True
+
+def get_password_hash(password, existing_hash):
+    # return calculated hash
+    try:
+       return subprocess.run(['perl', '-e', f"print crypt('{password}', '{existing_hash}')"], capture_output=True, text=True).stdout
+    except:
+        return None
 
 def request_job(cmd_info, job_command, cmd_step_no=1, depend_on_job_id=-1, has_dependent="N"):
     global jobs_requested
