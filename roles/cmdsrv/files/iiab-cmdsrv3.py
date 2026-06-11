92e6a2d8e8fd2e1e63a38f9028ffc1b2097e944f
tim-moody <tim@timmoody.com>
2026-06-11 09:44:07 -0400

    new usage

--- roles/cmdsrv/files/iiab-cmdsrv3.py
+++ roles/cmdsrv/files/iiab-cmdsrv3.py
@@ -3319,7 +3319,7 @@ def calc_passwd_hash(password, existing_hash):
     # return calculated hash
     try:
         #return subprocess.run(['perl', '-e', f"print crypt('{password}', '{existing_hash}')"], capture_output=True, text=True).stdout
-        return subprocess.run(['scripts/perl-crypt.pl', password, existing_hash], capture_output=True, text=True).stdout
+        return subprocess.run(['perl', 'scripts/perl-crypt.pl', password, existing_hash], capture_output=True, text=True).stdout
     except:
         return None
 
